fe71adb397ad8213fad5a2e861dad878b585da9e
Colin Roy-Ehri <colin.roy.ehri@bytecode.io>
2021-02-20 10:29:44 -0700

    Initial commit

---
+++ src/visualizations/gauge.ts
@@ -0,0 +1,147 @@
+import { Looker, VisualizationDefinition } from "../common/types";
+import {
+  handleErrors,
+  getMinMaxDatetimes,
+  processQueryResponse,
+  gaugeOptions,
+} from "../common/utils";
+import { fontFamily } from "../common/chart-defaults";
+import { Chart, Options } from "highcharts";
+import { Highcharts } from "highcharts-more-node";
+
+declare var looker: Looker;
+
+interface GaugeViz extends VisualizationDefinition {
+  elementRef?: HTMLDivElement;
+}
+
+const vis: GaugeViz = {
+  id: "gauge-chart", // id/label not required, but nice for testing and keeping manifests in sync
+  label: "gauge-chart",
+  options: {
+    color: {
+      type: "array",
+      label: "Colors",
+      display: "colors",
+      default: [
+        "#75E2E2",
+        "#3EB0D5",
+        "#4276BE",
+        "#592EC2",
+        "#9174F0",
+        "#B1399E",
+        "#B32F37",
+        "#E57947",
+        "#FBB555",
+        "#FFD95F",
+        "#C2DD67",
+        "#72D16D",
+      ].map((value) => {
+        return value.toLowerCase();
+      }),
+    },
+    title: {
+      type: "string",
+      label: "Chart(s) with Navigator",
+      display: "text",
+      default: "Chart(s) with Navigator",
+    },
+    charts: {
+      type: "string",
+      label: "Type of Chart(s)",
+      display: "select",
+      values: [{ "Gauge Chart": "gauge" }],
+      default: "gauge",
+    },
+    overlay: {
+      type: "boolean",
+      label: "Overlay",
+      display: "checkbox",
+      default: true,
+    },
+  },
+  // Set up the initial state of the visualization
+  create(element, config) {
+    element.className = "highcharts-custom-vis";
+  },
+  // Render in response to the data or settings changing
+  update(data, element, config, queryResponse) {
+    // console.log("data", data);
+    // console.log("element", element);
+    // console.log("config", config);
+    console.log("queryResponse", queryResponse);
+    const filterMin =
+      queryResponse &&
+      queryResponse.applied_filters &&
+      queryResponse.applied_filters["analytics_func.gauge_lower_threshold"] &&
+      queryResponse.applied_filters["analytics_func.gauge_lower_threshold"]
+        .value;
+    const filterMax =
+      queryResponse &&
+      queryResponse.applied_filters &&
+      queryResponse.applied_filters["analytics_func.gauge_upper_threshold"] &&
+      queryResponse.applied_filters["analytics_func.gauge_upper_threshold"]
+        .value;
+    console.dir(`lower threshold: ${filterMin}`);
+    console.dir(`lower threshold: ${filterMax}`);
+
+    const errors = handleErrors(this, queryResponse, {
+      min_pivots: 0,
+      max_pivots: 0,
+      min_dimensions: 1,
+      max_dimensions: 5,
+      min_measures: 1,
+      max_measures: 1,
+    });
+
+    let [pivots, dimensions, measures] = processQueryResponse(queryResponse);
+    let fields = dimensions.concat(measures);
+    let timeSeries = fields.filter((field) => field.type?.includes("date"));
+
+    if (timeSeries.length > 1) {
+      console.log(
+        "More than one date dimension or measure was found. Only one date dimension or measure is supported for time series data."
+      );
+    }
+
+          
+    let derivedMin = Math.min(
+      ...data.map((x) => {
+        return 1.0 * x[measures[0].name].value;
+      })
+    );
+    let minValue = 0
+    if (filterMin) {
+      minValue = filterMin
+    } else if (derivedMin) {
+      minValue = derivedMin
+    } else if (filterMin === 0) {
+      minValue = 0
+    }
+    
+    let derivedMax = Math.max(
+      ...data.map((x) => {
+        return 1.0 * x[measures[0].name].value;
+      })
+    );
+    let maxValue = 0
+    if (filterMax) {
+      maxValue = filterMax
+    } else if (derivedMax) {
+      maxValue = derivedMax
+    } else if (filterMax === 0) {
+      maxValue = 0
+    }
+    
+
+    const [minTime, maxTime, maxIndex] = getMinMaxDatetimes(data, timeSeries);
+
+    const latest = data[maxIndex][measures[0].name].value;
+    const title = data[maxIndex][measures[0].name].rendered;
+   
+    const options = gaugeOptions(minValue, maxValue, latest, fontFamily, title);
+    Highcharts.chart(element, options);
+  },
+};
+
+looker.plugins.visualizations.add(vis);
